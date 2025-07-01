45bf488406510e0141b457d0d4e1dc85894a71ff
Infohubsite <gigwadorusdominante@gmail.com>
2025-07-01 18:36:51 +0300

    maybe fixed some shit

--- Services/AuthenticationHeaderHandler.cs
+++ Services/AuthenticationHeaderHandler.cs
@@ -3,13 +3,22 @@
     public class AuthenticationHeaderHandler(ILocalStorageService localStorage) : DelegatingHandler
     {
         private readonly ILocalStorageService _localStorage = localStorage;
+        private readonly SemaphoreSlim _semaphore = new(1, 1);
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            string token = await this._localStorage.GetItemAsync<string>("authToken");
-            if (!string.IsNullOrEmpty(token))
-                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-            return await base.SendAsync(request, cancellationToken);
+            await this._semaphore.WaitAsync(cancellationToken);
+            try
+            {
+                string token = await this._localStorage.GetItemAsync<string>("authToken");
+                if (!string.IsNullOrEmpty(token))
+                    request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                return await base.SendAsync(request, cancellationToken);
+            }
+            finally
+            {
+                this._semaphore.Release();
+            }
         }
     }
 }
