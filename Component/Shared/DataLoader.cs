ea8e36a56fca7bca03de43c3502d9da1c136ced8
HungryMuttis <216588469+Infohubsite@users.noreply.github.com>
2025-12-27 20:00:51 +0200

    added a dataloader component

---
+++ Component/Shared/DataLoader.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Frontend.Component.Shared
+{
+    public class DataLoader : DataLoaderBase
+    {
+        [Parameter, EditorRequired] public Func<bool, Task<List<string>>> LoadAction { get; set; }
+
+        public Task Refresh() => ExecuteLoad(true);
+
+        protected override Task OnInitializedAsync() => ExecuteLoad(false);
+
+        private async Task ExecuteLoad(bool refresh)
+        {
+            StartLoading();
+            try
+            {
+                FinishLoading(await LoadAction(refresh));
+            }
+            catch (Exception ex)
+            {
+                FinishLoading([$"Error while loading: {ex.Message}"]);
+            }
+        }
+    }
+
+    public class DataLoaderT<TArg> : DataLoaderBase
+    {
+        [Parameter, EditorRequired] public Func<bool, TArg?, Task<List<string>>> LoadAction { get; set; }
+
+        [Parameter] public TArg? InitialArg { get; set; }
+
+        public Task Refresh(TArg? arg) => ExecuteLoad(true, arg);
+
+        protected override Task OnInitializedAsync() => ExecuteLoad(false, InitialArg);
+
+        private async Task ExecuteLoad(bool refresh, TArg? arg)
+        {
+            StartLoading();
+            try
+            {
+                FinishLoading(await LoadAction(refresh, arg));
+            }
+            catch (Exception ex)
+            {
+                FinishLoading([$"Error while loading: {ex.Message}"]);
+            }
+        }
+    }
+}
---
+++ Component/Shared/DataLoaderBase.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Frontend.Component.Shared
+{
+    public abstract partial class DataLoaderBase
+    {
+        [Parameter] public bool DisplaySpinner { get; set; } = true;
+        [Parameter] public bool DisplayAnyway { get; set; } = false;
+
+        [Parameter] public RenderFragment? Loading { get; set; }
+        [Parameter] public RenderFragment? Loaded { get; set; }
+        [Parameter] public RenderFragment? ChildContent { get; set; }
+
+        private bool _isLoading = true;
+        private List<string> _errors = [];
+
+        protected void StartLoading()
+        {
+            _errors = [];
+            _isLoading = true;
+            StateHasChanged();
+        }
+        protected void FinishLoading(List<string> errors)
+        {
+            _errors = errors;
+            _isLoading = false;
+            StateHasChanged();
+        }
+    }
+}
